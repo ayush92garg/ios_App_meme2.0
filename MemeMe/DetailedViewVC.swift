4d209c2f4304be2122976b75f11c613440b32ccb
Ayush Garg <ayushgarg@Ayushs-MacBook-Pro.local>
2016-10-25 00:54:48 +0530

    added edit button in detailViewVC

--- MemeMe/DetailedViewVC.swift
+++ MemeMe/DetailedViewVC.swift
@@ -10,51 +10,46 @@ import UIKit
 
 class DetailedViewVC: UIViewController {
     
-    var labelText: String?
+    var topText: String?
+    var bottomText: String?
     var finalImage:  UIImage?
+    var originalImage: UIImage?
     
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var image: UIImageView!
     
-    @IBAction func doneButtonClicked(){
-//        let tableView = self.storyboard!.instantiateViewController(withIdentifier: "TableViewVC") as! TableViewVC
-//        self.navigationController?.popToViewController(tableView, animated: true)
-         self.dismiss(animated: true, completion: nil)
-    }
-    
     
     override func viewWillAppear(_ animated: Bool) {
-        if let labelText = labelText {
-            label.text = labelText
+        
+        self.tabBarController?.tabBar.isHidden = true
+        self.navigationItem.rightBarButtonItem = editButtonItem
+        editButtonItem.action = #selector(DetailedViewVC.editButtonPressed)
+        
+        
+        if let topText = topText {
+            if let bottomText = bottomText {
+                label.text = topText + " " + bottomText
+            }
         }
         
         if let finalImage = finalImage{
             image.image = finalImage
         }
-//        label.text = labelText
-//        image.image = finalImage
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func editButtonPressed() {
+        let editMeme = self.storyboard?.instantiateViewController(withIdentifier: "CreateMemeVC") as! CreateMemeVC
+        print(originalImage)
+        editMeme.origTopText = topText!
+        editMeme.origBottomText = bottomText!
+        editMeme.origImage = originalImage!
+        editMeme.edit = true
+        self.present(editMeme, animated: false, completion: {
+            print("presented")
+            
+        })
     }
-    */
+
+
 
 }
